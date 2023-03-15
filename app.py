1e65827b3c1a9e46d7879933b227a1f617881f24
HeljaeRaeisaenen <94612974+HeljaeRaeisaenen@users.noreply.github.com>
2023-03-15 18:18:08 +0200

    practice, creating base of the webpage

--- app.py
+++ app.py
@@ -1,17 +1,48 @@
 from flask import Flask
-from flask import render_template, request
+from flask import render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
+from dotenv import dotenv_values
+
+URI = dotenv_values('.env')['DATABASE_URI']
 
 app = Flask(__name__)
+print(URI)
+app.config['SQLALCHEMY_DATABASE_URI'] = URI
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    result = db.session.execute(text("SELECT title,time,id FROM posts"))
+    posts = result.fetchall()
+    return render_template("index.html", posts=posts)
 
 @app.route("/login")
 def loginpage():
     return render_template("login.html")
 
-
-@app.route("/login", methods=["POST"])
-def login():
+@app.route("/", methods=["POST"])
+def loginaction():
     return render_template("userpage.html", username=request.form["username"])
+
+@app.route("/keskustelu/<int:id>")
+def discussion(id):
+    sql = text("SELECT * FROM posts WHERE id=:id")
+    result = db.session.execute(sql, {"id":id})
+    post = result.fetchone()
+    return render_template("discussion.html", post=post)
+
+@app.route("/uusi")
+def new_post():
+    return
+
+@app.route("/create", methods=["POST"])
+def create():
+    title = request.form["title"]
+    message = request.form["message"]
+
+    sql = text("INSERT INTO posts (title,message,time) VALUES (:title,:message,NOW()) RETURNING id")
+    result = db.session.execute(sql, {"title":title, "message":message})
+    post_id = result.fetchone()[0]
+    db.session.commit()
+    return redirect(f"/keskustelu/{post_id}")
