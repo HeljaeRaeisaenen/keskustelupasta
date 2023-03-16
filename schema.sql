98e58daf9c06c547b7b0d958a0fcc0ccb4b3dfa0
HeljaeRaeisaenen <94612974+HeljaeRaeisaenen@users.noreply.github.com>
2023-03-16 12:54:37 +0200

    created schema

---
+++ schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT, passwordhash TEXT);
+
+CREATE TABLE posts (id SERIAL PRIMARY KEY, title TEXT, message TEXT, time TIMESTAMP, user_id INTEGER REFERENCES users, topic_id INTEGER REFERENCES topics);
+
+CREATE TABLE comments (id SERIAL PRIMARY KEY, message TEXT, time TIMESTAMP, user_id INTEGER REFERENCES users, post_id INTEGER REFERENCES posts);
+
+CREATE TABLE topics (id SERIAL PRIMARY KEY, topic TEXT);
+
+CREATE TABLE admins (id SERIAL PRIMARY KEY, username TEXT, passwordhash TEXT);
+
+
