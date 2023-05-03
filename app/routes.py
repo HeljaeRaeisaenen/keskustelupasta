5e6571d323136c6fe7fb9c9d08dbd55c278e8506
HeljaeRaeisaenen <94612974+HeljaeRaeisaenen@users.noreply.github.com>
2023-05-03 18:03:49 +0300

    fixed a security flaw

--- app/routes.py
+++ app/routes.py
@@ -201,10 +201,14 @@ def userpage(username):
     if username == "deleted user":
         abort(404)
     user_posts = posts.get_by_user(users.find_user_id(username))
-    return render_template("user_page.html",
-                           user_posts=user_posts,
-                           username=username)
-
+    
+    response = make_response(render_template(
+        "user_page.html",
+        user_posts=user_posts,
+        username=username
+    ))
+    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    return response
 
 @app.route("/deleteuser", methods=["POST"])
 def delete_user():
